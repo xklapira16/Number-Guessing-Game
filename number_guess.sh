d35fe29885b5f6b631bab533fac1317508c26e00
xklapira16 lapira <xk.lapira@gmail.com>
2025-02-27 13:56:31 +0000

    Initial commit

---
+++ number_guess.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Database connection variable
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+# Create the database if it doesn't exist
+$PSQL "CREATE TABLE IF NOT EXISTS users (username VARCHAR(22) PRIMARY KEY, games_played INT DEFAULT 0, best_game INT DEFAULT NULL);"
+
+# Prompt for username
+read -p "Enter your username: " USERNAME
+
+# Check if the user exists
+USER_INFO=$($PSQL "SELECT games_played, best_game FROM users WHERE username = '$USERNAME';")
+
+if [[ -z $USER_INFO ]]; then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  # Insert new user into the database
+  $PSQL "INSERT INTO users (username) VALUES ('$USERNAME');"
+else
+  GAMES_PLAYED=$(echo $USER_INFO | cut -d '|' -f 1)
+  BEST_GAME=$(echo $USER_INFO | cut -d '|' -f 2)
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+# Generate a random number between 1 and 1000
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+NUMBER_OF_GUESSES=0
+
+# Start guessing loop
+while true; do
+  read -p "Guess the secret number between 1 and 1000: " GUESS
+
+  # Check if the input is an integer
+  if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+
+  ((NUMBER_OF_GUESSES++))
+
+  if [[ $GUESS -lt $SECRET_NUMBER ]]; then
+    echo "It's higher than that, guess again:"
+  elif [[ $GUESS -gt $SECRET_NUMBER ]]; then
+    echo "It's lower than that, guess again:"
+  else
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    # Update the user's game stats
+    if [[ -z $BEST_GAME || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]; then
+      $PSQL "UPDATE users SET games_played = games_played + 1, best_game = $NUMBER_OF_GUESSES WHERE username = '$USERNAME';"
+    else
+      $PSQL "UPDATE users SET games_played = games_played + 1 WHERE username = '$USERNAME';"
+    fi
+    break
+  fi
+done
